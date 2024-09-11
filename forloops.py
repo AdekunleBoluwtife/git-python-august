3ca76dbc72e00ce8baff871d9f292b825d8834bc
Adekunle Adebowale Boluwatife <adebowale996@gmail.com>
2024-09-11 13:28:06 +0100

    add forloops file

---
+++ forloops.py
@@ -0,0 +1,37 @@
+# states = ["Oyo", "Lagos", "Kanduna", "Gombe", "Plateau"]
+
+# for state in states:
+# #     print(state)
+
+# items = ("guitar", "Keyboard", "drums", "microphone", "amp")
+
+# for item in items:
+#     print(item)
+
+# states_and_capitals = {"Abia": "Umuahia", "Adamawa": "Yola", "Yobe": "Damataru", "Taraba": "Jalingo", "Anambra": "Awka"}
+
+# for states, capital in states_and_capitals.items():
+#     print(f"The capital of {states} is {capital}")
+
+# my_string = "Python is fun!"
+
+# for char in my_string:
+# #     print(char)
+
+# for i in range(1,10,2):
+#     print(i)
+
+
+items = ("Bag", "Caps", "Bucket")
+
+for i, item in enumerate(items, start=1):
+    print(f"Item {i} is {item}")
+
+# fruits = ['Apple', 'Banana', 'Cherry']
+# fruits_copy = [fruit for fruit in fruits]
+# print(fruits_copy)
+
+# nums = [1,4,5,10,12,67]
+# even_nums = [num for num in nums if num % 2 == 0]
+# print(even_nums)
+
